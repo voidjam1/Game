6f4d779992ebc560dc5edbd72e5c20087de5c6bb
voidjam1 <v01dj4m@gmail.com>
2026-02-18 13:51:32 +0900

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,6 @@
+window.onerror = function(msg, url, line) {
+  alert("错误信息: " + msg + "\n文件: " + url + "\n行号: " + line);
+};
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App'
